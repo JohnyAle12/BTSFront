0132b97645d27699e8820865c1d1f9f5429bb898
Johny Prieto <johnyprieto001@gmail.com>
2023-01-29 21:22:10 -0500

    fix some views with best css

--- src/auth/views/Login.tsx
+++ src/auth/views/Login.tsx
@@ -34,38 +34,44 @@ export const Login = () => {
                 <div className="fadeIn first">
                     <div className="m-4">
                         <img src={reactLogo} className="logo react" alt="React logo" />
-                        <h3 id="titleLogin">Weather App</h3>
+                        <h3 id="titleLogin" className="mt-3">Weather App</h3>
                     </div>
                 </div>
 
-                <form onSubmit={onLoginSubmit}>
-                    <input
-                        type="text"
-                        className="fadeIn second"
-                        name="email"
-                        placeholder="Correo electrónico"
-                        autoComplete="off"
-                        value={email}
-                        onChange={onInputChange}
-                        required
-                    />
-                    <input
-                        type="password"
-                        className="fadeIn second"
-                        name="password"
-                        placeholder="Contraseña"
-                        autoComplete="off"
-                        value={password}
-                        onChange={onInputChange}
-                        required
-                    />
-                    <input type="submit" className="fadeIn fourth" value="Iniciar sesión" />
-                </form>
-                {loginFailed && (
-                        <div className="alert alert-danger" role="alert">
-                            Error al intentar autenticar
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <form onSubmit={onLoginSubmit}>
+                                <input
+                                    type="text"
+                                    className="form-control fadeIn second"
+                                    name="email"
+                                    placeholder="Correo electrónico"
+                                    autoComplete="off"
+                                    value={email}
+                                    onChange={onInputChange}
+                                    required
+                                />
+                                <input
+                                    type="password"
+                                    className="form-control mt-3 fadeIn second"
+                                    name="password"
+                                    placeholder="Contraseña"
+                                    autoComplete="off"
+                                    value={password}
+                                    onChange={onInputChange}
+                                    required
+                                />
+                                <input type="submit" className="btn btn-secondary btn-block mt-3 mb-3 fadeIn fourth" value="Iniciar sesión" />
+                            </form>
+                            {loginFailed && (
+                                <div className="alert alert-danger" role="alert">
+                                    Error al intentar autenticar
+                                </div>
+                            )}
                         </div>
-                    )}
+                    </div>
+                </div>
 
                 <div id="formFooter"></div>
             </div>
